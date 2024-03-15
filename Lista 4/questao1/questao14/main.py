5ace278fcc010b60fcd6884b36e58e94459df8e8
lucasmotareis <lucasmotareis@gmail.com>
2024-03-15 20:19:11 -0300

    Lista 4\questao1\

---
+++ Lista 4/questao1/questao14/main.py
@@ -0,0 +1,8 @@
+from func import *
+
+lista = []
+i=0
+numero_de_ramdons = int(input("Digite o tamanho da lista aleatória: "))
+valor_desejado = int(input("Digite o valor que quer proucurar na lista: "))
+
+print(pesquisa_binaria(listaAleatoria(numero_de_ramdons),valor_desejado))
